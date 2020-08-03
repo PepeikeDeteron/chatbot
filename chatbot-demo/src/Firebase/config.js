2b13d7cc5205f11e5a0afc395c4c0a269bb95160
Brainfuck <sasa_yoshi@docomo.ne.jp>
2020-08-03 23:23:12 +0900

    :wrench: Firestore を使う準備

---
+++ chatbot-demo/src/Firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDi9pnVhkYX7O94y-g1gL5YbvhJNbcpWBA",
+  authDomain: "chatbot-demo-bcf09.firebaseapp.com",
+  databaseURL: "https://chatbot-demo-bcf09.firebaseio.com",
+  projectId: "chatbot-demo-bcf09",
+  storageBucket: "chatbot-demo-bcf09.appspot.com",
+  messagingSenderId: "231306222957",
+  appId: "1:231306222957:web:04944411e86493ecbcce64",
+  measurementId: "G-XWKX1VEXG9"
+};
+
+export default firebaseConfig;
